64dec48a74c84423ec7b37508c9fbf2a34787f94
AlexRebega <rebega.alex@yahoo.com>
2015-06-24 12:36:08 -0700

    Finding max and min grades from a file containing list of students and results. If there are more min|max grades only prints one of the students - to be fixed

--- min-and-max.py
+++ min-and-max.py
@@ -23,9 +23,37 @@ def main():
 		print >> sys.stderr, "Argument is not a valid filename"
 		usage()
 		sys.exit(1)
+	students = []
+	for line in list(fp):
+		#print "%d : %s" %(idx+1, line),
+		tokens = line.strip().split(';')
+		#print tokens
+		student = {
+		'first_name': tokens[0],
+		'last_name': tokens[1],
+		'group': tokens[2],
+		'grade': int(tokens[3])
+		}
 
-	for idx, line in enumerate(list(fp)):
-		print "%d : %s" %(idx+1, line),
+		students.append(student)
+	#print students
 
+	grade_min = 11;
+	grade_max = -1;
+	student_bun = []
+	student_prost = []
+
+	for s in students:
+		if grade_max < s['grade']:
+			grade_max = s['grade']
+			student_bun = s 
+		if grade_min > s['grade']:
+			grade_min = s['grade']
+			student_prost = s 
+	print "Min grade is: %d", grade_min
+	print "Max grade is: %d", grade_max
+	print "Best student :", student_bun
+	print "Wors fuking student:", student_prost
+	
 if __name__ == "__main__":
 	sys.exit(main())
